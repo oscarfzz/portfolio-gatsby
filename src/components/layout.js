a74727abcf97369db78e26f59182eb94de755171
Danseks <danseksfzz@gmail.com>
2021-04-03 10:15:13 +0200

    Change style and add more functions

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,10 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import styled from "@emotion/styled"
 import Navbar from "./Navbar/Navbar"
-import GlobalStyle from "../styles/GlobalStyle"
 
 const Layout = ({ isHomePage }) => {
   return (
     <>
-      <GlobalStyle />
       <Navbar />
       {isHomePage ? <div>holi</div> : null}
     </>
