5efc6871cadb0d00003dd7664a54c82a3f0e0cf6
Danseks <danseksfzz@gmail.com>
2021-04-08 15:28:24 +0200

    404

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
         host: "oscarfdz.com", // if 'www' is set to 'false', be sure to also remove it here!
         ErrorDocument: `
           ErrorDocument 401 /error_pages/401.html
-          ErrorDocument 404 /404/index.html
+          ErrorDocument 404 404.html
           ErrorDocument 500 /error_pages/500.html
         `,
       },
